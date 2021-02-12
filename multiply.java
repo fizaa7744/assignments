471fe0e2b63835eaefed80803a6962a336193321
fizaa7744 <73763744+fizaa7744@users.noreply.github.com>
2021-02-12 10:20:30 +0500

    Add files via upload

---
+++ multiply.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class multiply{
+public static void main(String[] args)
+{ int a=10;
+   int b=5;
+    int c;
+    c=a*b;
+System.out.println("the product of a and b is:");
+System.out.println(c);
+}
+}
